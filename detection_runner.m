ed7818d7745f615c382deef456fcf27ad7d8c4f5
Alex <yluo6@uw.edu>
2022-02-22 15:41:23 -0800

    Improve handling of multiple detections & bbox filtering

--- detection_runner.m
+++ detection_runner.m
@@ -15,29 +15,57 @@ function detection_runner()
 
 startup;
 
-fprintf('compiling the code...');
-compile;
-fprintf('done.\n\n');
+% Disable compilation after first run
+
+% fprintf('compiling the code...');
+% compile;
+% fprintf('done.\n\n');
 
 % load model
 load('VOC2010/person_final');
-% model.vis = @() visualizemodel(model, ...
-%                   1:2:length(model.rules{model.start})); %#ok<NODEF>
-% load('VOC2010/person_grammar_final');
-% model.class = 'person grammar';
-% model.vis = @() visualize_person_grammar_model(model, 6);
 
 % Process a single image
-% detect('/home/alex/Pictures/0000000146.jpg', model, 1, '146.jpg');
+% detect('/Users/alex/Research/Data/0000000003.jpg', model, 1, '3.jpg');
 
 
 % Process all images in a folder
-files = dir('/Users/alex/Research/Data/images_2/*.jpg');
-for k = 1:length(files)
-    detect(join(['/Users/alex/Research/Data/images_2/',files(k).name]), model, 1, files(k).name);
-     disp("### "+ k + "/" + length(files) + "    " + files(k).name);
-end
+% files = dir('/Users/alex/Research/Data/thres_samples/*.jpg');
+% fileID = join(['/Users/alex/Research/thres_output', '/bbox.csv'], '');
+% % disp(repmat([5],[3 1]));
+% for k = 1:length(files)
+% %     writematrix(files(k).name, fileID, 'WriteMode', 'append');
+%     box = detect(join(['/Users/alex/Research/Data/thres_samples/',files(k).name]), model, 1, files(k).name);
+%     % Append a column in front representing the file name
+%     first_col = repmat(str2num(files(k).name(1:end-4)), [height(box) 1]);
+%     writematrix([first_col, round(box, 3)], fileID, 'WriteMode', 'append');
+%     disp("### "+ k + "/" + length(files) + "    " + files(k).name);
+% end
 
+% Process images for a given date
+    samples_dir = "/Users/alex/Research/Image Tests/";
+    samples_list = dir(join(samples_dir, "*"));
+    % Match directories with name matching 
+    % YYYY_MM_DD_{{specific description of the entry}}
+    entry_pattern = digitsPattern(4) + "_" + digitsPattern(2) + "_" + digitsPattern(2) + "_" + wildcardPattern;
+    match_entry = matches({samples_list.name}, entry_pattern);
+    for j = 1:length(samples_list)
+        if match_entry(j)
+            % Build the full for the directory of a specific data entry
+            entry_dir = join([samples_dir, samples_list(j).name, '/'], '');
+            disp(join(["Processing: ", entry_dir], ''));
+            images_list = dir(join([entry_dir, '*.jpg'], ''));
+            fileID = join([entry_dir, '/bbox.csv'], '');
+            for l = 1:length(images_list)
+                box = detect(join([entry_dir, '/',images_list(l).name], ''), model, entry_dir, images_list(l).name);
+                % Append a column in front representing the file name
+                first_col = repmat(str2double(images_list(l).name(1:end-4)), [height(box) 1]);
+                writematrix([first_col, round(box, 3)], fileID, 'WriteMode', 'append');
+            end
+            fclose('all');
+        end
+    end
+
+fclose('all');
 
 % % Process data
 % files2 = dir('/home/alex/Pictures/*');
@@ -57,14 +85,13 @@ end
 
 
 
-
-
-
 % detection runner
-function detect(imname, model, num_dets, name)
+function out = detect(imname, model, out_dir, name)
 cls = model.class;
 
 im = imread(imname);
+im = imresize(im, [480 NaN]);
+
 clf;
 image(im);
 axis equal; 
@@ -72,63 +99,58 @@ axis on;
 
 % output file name
 fname = extractBefore(name,'.jpg');
-folder = '/Users/alex/Research/test_output/';
+folder = join([out_dir, "with_box"], '');
 if ~exist(folder, 'dir')
   mkdir(folder);
 end
 
-% load and display model
-% model.vis();
-
 % detect objects
-[ds, bs] = imgdetect(im, model, -0.5);
-
+[ds, bs] = imgdetect(im, model, -0.5); %was -0.5
 
 
 % draw bounding box if there are valid detections
 if ~isempty(ds)
     top = nms(ds, 0.5);
-    top = top(1:min(length(top), num_dets));
+    top = top(1:min(length(top), 5));
     ds = ds(top, :);
     bs = bs(top, :);
     clf;
-
-    if model.type == model_types.Grammar
-      bs = [ds(:,1:4) bs];
-      % moved this inside so this will only run for Grammar models
-      
-%       disable image output
-%       showboxes(im, reduceboxes(model, bs), [join([folder,'/',fname])]);
-    end
     
-
     if model.type == model_types.MixStar
       % get bounding boxes
       bbox = bboxpred_get(model.bboxpred, ds, reduceboxes(model, bs));
       bbox = clipboxes(im, bbox);
-      top = nms(bbox, 13.5);
+      top = nms(bbox, 0.3);
       clf;
       
-      % Enforce  minimum size/ratio requirement to eliminate false detections
-      % At least 16000 pixels (80x200) and (height:width)>0.9
-      % TODO: Add the same code to detection_runner_text
-      w = bbox(top,3) - bbox(top,1);
-      h = bbox(top,4) - bbox(top,2);
+      % size check
+      w = bbox(:,3) - bbox(:,1);
+      h = bbox(:,4) - bbox(:,2);
       
-      if w * h < 16000 | h / w > 0.9
-        out = [-1 -1 -1 -1];
-        return;
+      % top2 holds the filtered list of top boxes
+      top2 = [];
+      
+      % check confidence level (threshold depends on box size)
+      for i = 1:length(top)
+          j = top(i);
+          if (bbox(j,5) < -0.35) || ((bbox(j,5) < 0.6) && (h(j) < 300) && (w(j) < 80))
+              disp(bbox(j,:));
+              continue;
+          end
+          top2 = [top2 j];
       end
+        
+      % output images
+      showboxes(im, bbox(top2,:), join([folder,'/',fname], ''));
+      out = bbox(top2,:);
 
-%       disable image output
-%       showboxes(im, bbox(top,:), [join([folder,'/',fname])]);
-      output_box(im, bbox(top,:));
     end
 % otherwise just export the image
 else
     clf;
-%     disable image output
-%     showboxes(im,[], [join([folder,'/',fname])]);
+    % output original image
+    showboxes(im,[], [join([folder,'/',fname])]);
+    out = [-1 -1 -1 -1 -1];
     disp("no detection in " + fname);
 end
 
